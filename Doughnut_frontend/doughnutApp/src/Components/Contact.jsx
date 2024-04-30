1a3240de6fa0b62dc71f2f09a63344556b070d20
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-04-30 15:17:07 +0530

    donut

---
+++ Doughnut_frontend/doughnutApp/src/Components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/contact.css'
+
+const Contact = () => {
+  return (
+    <div>
+        <div className='text'>
+            <p>Dulani</p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
