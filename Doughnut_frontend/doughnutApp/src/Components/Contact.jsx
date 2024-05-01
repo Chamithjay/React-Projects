13d8d347efe3f67f23b8e9ffebd36d7e1f0cb607
Chamitha Thambiliyagoda <chamithajayavihan2@gmail.com>
2024-05-01 13:46:43 +0530

    donut

--- Doughnut_frontend/doughnutApp/src/Components/Contact.jsx
+++ Doughnut_frontend/doughnutApp/src/Components/Contact.jsx
@@ -3,52 +3,92 @@ import './css/contact.css'
 import email from './email.svg';
 import call from './call.svg';
 import loc from './location.svg';
-import insta from './insta.png';
+import insta from './instagram.svg';
+import twit from './twitter.png';
+import fb from './fb.png';
+import donut from './donut.png';
 const Contact = () => {
     return (
         <div className='footer'>
             <table>
                 <tr>
-                    <td><b>Contact Us</b></td>
-                    <td><b>Visit our Office</b></td>
-                    <td><b>Follow Us</b></td>
+                    <td>
+                        <c><img src={donut}></img></c>
+                    </td>
+                    <td>
+                    <table>
+                        <tr>
+                            
+                            <td><b><a>Contact Us</a></b></td>
+                            <td><b><a>Visit our Office</a></b></td>
+                            <td><b><a>Follow Us</a></b></td>
+                            
+                        </tr>
+                        <tr>
+                            <td>
+                            <div className='logo'>
+                                <table>
+                                    <tr>                                    
+                                        <td><img src={email}></img></td>
+                                        <td><a href='https://www.youtube.com/'>info@doughnuts.com</a></td>                                        
+                                    </tr>
+                                </table>
+                            </div>
+                            </td>
+                            <td>
+                            <div className='logo'>
+                                <table>
+                                    <tr>                                    
+                                        <td><img src={loc}></img></td>
+                                        <td><a>846E, St Rita's avenue, Thumpeliya, Ja-Ela</a></td>                                        
+                                    </tr>
+                                </table>
+                            </div>
+                            </td>
+                            <td>
+                                <table >
+                                    <tr>
+                                        <div className='insta'>
+                                            <td><img src={insta}></img></td>
+                                            <td><img src={twit}></img></td>
+                                            <td><img src={fb}></img></td>
+                                            
+                                        </div>
+                                        
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                            <div className='logo'>
+                                <table>                                
+                                    <tr>
+                                        <td><img src={call}></img></td>
+                                        <td><a>+94 77 885 567</a></td>                                       
+                                    </tr>
+                                </table>
+                            </div>    
+                            </td>
+                        </tr>
+                    </table>            
+
+                    </td>
                 </tr>
-                <tr>
-                    <td>
-                        <table>
-                            <tr>
-                                <td><img src={email}></img></td>
-                                <td><a href='https://www.youtube.com/'>info@doughnuts.com</a></td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td>
-                        <table>
-                            <tr>
-                                <td><img src={loc}></img></td>
-                                <td><a>846E, St rita avenue, Thumpeliya, Ja-Ela</a></td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td>
-                        <table>
-                            <tr>
-                                <td><img src={insta}></img></td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <table>
-                            <tr>
-                                <td><img src={call}></img></td>
-                                <td><a>+94 77 885 567</a></td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>            
+            </table>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className='botnav'>             
+            <table>
+              <tr>
+                <td><a href='#'>Our Story</a></td>
+                <td><a href='#'>Our Products</a></td>
+                <td><a href='#'>Contact</a></td>  
+              </tr>
+            </table>       
+            </div>       
+            <hr></hr>
         </div>
   )
 }
